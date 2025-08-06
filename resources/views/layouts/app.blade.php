0d021ababde729ad419a89dd2319945a61d566c6
iriando <taufik.iriando@gmail.com>
2025-08-06 13:12:11 +0900

    ganti icon header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
   <meta name="keywords" content="">
 
   <!-- Favicons -->
-  <link href="img/favicon.png" rel="icon">
+  <link href="img/logo_bkn.png" rel="icon">
   <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Fonts -->
@@ -84,13 +84,13 @@
   <footer id="footer" class="footer position-relative light-background">
 
     <div class="container copyright text-center mt-4">
-      <p>© <span>Copyright</span> <strong class="px-1 sitename">QuickStart</strong><span>All Rights Reserved</span></p>
+      {{-- <p>© <span>Copyright</span> <strong class="px-1 sitename">QuickStart</strong><span>All Rights Reserved</span></p> --}}
       <div class="credits">
         <!-- All the links in the footer should remain intact. -->
         <!-- You can delete the links only if you've purchased the pro version. -->
         <!-- Licensing information: https://bootstrapmade.com/license/ -->
         <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        Designed by <a href="https://manokwari.bkn.go.id/">Kanreg XIV BKN Manokwari</a>
       </div>
     </div>
 
